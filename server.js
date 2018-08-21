9a96832b328022a182a2321a584a20d158c493f6
designerseyi <40296336+designerseyi@users.noreply.github.com>
2018-08-21 22:25:43 +0000

    working

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require('express'); // ExperssJS Framework
 var app = express(); // Invoke express to variable for use in application
-var port = process.env.PORT || 8080; // Set default port or assign a port in enviornment
+var port = process.env.PORT || 8000; // Set default port or assign a port in enviornment
 var morgan = require('morgan'); // Import Morgan Package
-var mongoose = require('mongoose'); // HTTP request logger middleware for Node.js
+// var mongoose = require('mongoose'); // HTTP request logger middleware for Node.js
 var bodyParser = require('body-parser'); // Node.js body parsing middleware. Parses incoming request bodies in a middleware before your handlers, available under req.body.
 var router = express.Router(); // Invoke the Express Router
 var appRoutes = require('./app/routes/api')(router); // Import the application end points/API
 var path = require('path'); // Import path module
-var passport = require('passport'); // Express-compatible authentication middleware for Node.js.
-var social = require('./app/passport/passport')(app, passport); // Import passport.js End Points/API
-
+// var passport = require('passport'); // Express-compatible authentication middleware for Node.js.
+// var social = require('./app/passport/passport')(app, passport); // Import passport.js End Points/API
+var firebase = require("firebase");
 app.use(morgan('dev')); // Morgan Middleware
 app.use(bodyParser.json()); // Body-parser middleware
 app.use(bodyParser.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
@@ -19,19 +19,37 @@ app.use('/api', appRoutes); // Assign name to end points (e.g., '/api/management
 //
 // <---------- REPLACE WITH YOUR MONGOOSE CONFIGURATION ---------->
 //
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/Citykruise',  function(err) {
-    if (err) {
-        console.log('Not connected to the database: ' + err); // Log to console if unable to connect to database
-    } else {
-        console.log('Successfully connected to MongoDB'); // Log to console if able to connect to database
-    }
-});
+
+// mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://localhost:27017/Citykruise', function(err) {
+//     if (err) {
+//         console.log('Not connected to the database: ' + err); // Log to console if unable to connect to database
+//     } else {
+//         console.log('Successfully connected to MongoDB'); // Log to console if able to connect to database
+//     }
+// });
+
+
+
+// var config = {
+//   apiKey: "<API_KEY>",
+//   authDomain: "<PROJECT_ID>.firebaseapp.com",
+//   databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+//   storageBucket: "<BUCKET>.appspot.com",
+// };
+// firebase.initializeApp(config);
+
 
 
 // Set Application Static Layout
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/app/views/index.html')); // Set index.html as layout
 });
+
+
+// app.get('/main', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/public/app/views/pages/users/mainApp/index.html')); // Set index.html as layout
+// });
 
 
 // Start Server
